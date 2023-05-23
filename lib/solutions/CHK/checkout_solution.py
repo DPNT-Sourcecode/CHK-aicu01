699e618bf5ed5d37c1813fefb2a608e4a1f99ccb
sbx_user1051 <sbx_user1051@169.254.250.149>
2023-05-23 22:26:17 +0000

    Tue May 23 22:26:17 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -41,8 +41,12 @@ def calculate_cost(item, count, skus):
                     count_free_item = skus.count(offer['free'])
                     if count_free_item > 0:
                         skus = skus.replace(offer['free'], '', 1)
+    if not any(offer.get('free') == item for o in offers.values() for offer in o):
+        total_cost += count * items[item]
 
     total_cost += count * items[item]
     
     return total_cost, skus
+
+print(checkout('EEB'))
 
