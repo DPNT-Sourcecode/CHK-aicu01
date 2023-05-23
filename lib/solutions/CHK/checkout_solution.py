afef2017fcd58df9dec17c4da0d328b761b21332
sbx_user1051 <sbx_user1051@169.254.250.149>
2023-05-23 21:07:42 +0000

    Tue May 23 21:07:42 UTC 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,16 @@
 
 
+items = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
+offers = {'A': {'count': 3, 'price': 130},
+          'B': {'count': 2, 'price': 45}}
+
+
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    if skus not in items.keys():
+        return -1
+    
+
+    
+
